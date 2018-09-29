537abfc2f88b421f8318e99a36f5e9ea6a76c097
Gorbunov <darkwen3@mail.ru>
2018-09-29 20:53:33 +0300

    easy

--- matrix.resize.c
+++ matrix.resize.c
@@ -1,5 +1,3 @@
-#include <stdlib.h>
-
 #include "matrix.h"
 
 int min(int a, int b)
@@ -20,8 +18,8 @@ int matrix_resize(MATRIX* matrix, int rows, int cols)
     if (m == NULL)
         return ALLOC_ERROR;
 
-    for (int i = 0; i < min(rows, ((int*)matrix)[ROW_OFFSET]); i++)
-        for (int j = 0; j < min(cols, ((int*)matrix)[COL_OFFSET]); j++)
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < cols; j++)
             matrix_set(m, i, j, matrix_get(*matrix, i, j));
 
     matrix_delete(*matrix);
