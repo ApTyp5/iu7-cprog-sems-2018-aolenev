e420f80cee0ce8d074c7620d9576c899097ffcc5
Gorbunov <darkwen3@mail.ru>
2018-09-29 21:04:31 +0300

    easy [2]

--- matrix.resize.c
+++ matrix.resize.c
@@ -7,19 +7,24 @@ int min(int a, int b)
 
 int matrix_resize(MATRIX* matrix, int rows, int cols)
 {
+    int row_1 = 0;
+    int col_1 = 0;
+    
     if (matrix == NULL)
         return NULLPTR_ERR;
 
     if (rows <= 0 || cols <= 0)
         return INVALID_DIMENSIONS_ERR;
 
+    matrix_size(*matrix, &row_1, &col_1);
+    
     MATRIX m = matrix_create(rows, cols);
 
     if (m == NULL)
         return ALLOC_ERROR;
 
-    for (int i = 0; i < rows; i++)
-        for (int j = 0; j < cols; j++)
+    for (int i = 0; i < row_1; i++)
+        for (int j = 0; j < col_1; j++)
             matrix_set(m, i, j, matrix_get(*matrix, i, j));
 
     matrix_delete(*matrix);
