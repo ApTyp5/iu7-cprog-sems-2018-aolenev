faa3075d2cc4b88b3b4a896ccecb88b5e66cb99b
Drozdov Vladimir <drozdovvd@student.bmstu.ru>
2018-09-30 19:48:44 +0300

    Switched to calling matrix_size() instead of accessing sizes by their offsets.

--- matrix.resize.c
+++ matrix.resize.c
@@ -7,8 +7,8 @@ int min(int a, int b)
 
 int matrix_resize(MATRIX* matrix, int rows, int cols)
 {
-    int row_1 = 0;
-    int col_1 = 0;
+    int old_rows = 0;
+    int old_cols = 0;
     
     if (matrix == NULL)
         return NULLPTR_ERR;
@@ -16,15 +16,15 @@ int matrix_resize(MATRIX* matrix, int rows, int cols)
     if (rows <= 0 || cols <= 0)
         return INVALID_DIMENSIONS_ERR;
 
-    matrix_size(*matrix, &row_1, &col_1);
+    matrix_size(*matrix, &old_rows, &old_cols);
     
     MATRIX m = matrix_create(rows, cols);
 
     if (m == NULL)
         return ALLOC_ERROR;
 
-    for (int i = 0; i < row_1; i++)
-        for (int j = 0; j < col_1; j++)
+    for (int i = 0; i < min(rows, old_rows); i++)
+        for (int j = 0; j < min(cols, old_cols); j++)
             matrix_set(m, i, j, matrix_get(*matrix, i, j));
 
     matrix_delete(*matrix);
