baff638b00607558e0cb54e01b73743442ff41ed
Stepanov Alexandr <happymainer@gmail.com>
2018-10-13 19:04:05 +0300

    Add checks for reverce

--- sem_03_strIU7ng/striu7ng.reverse.c
+++ sem_03_strIU7ng/striu7ng.reverse.c
@@ -2,13 +2,22 @@
 
 strIU7ng reverse(strIU7ng string)
 {
+    if (string == NULL)
+        return NULL;
+
+    if (string.head == NULL)
+        return NULL;
+
+    if (string.len == 0)
+        return strIU7ng_create("");
+
     char *result = malloc(string.len);
 
     if (result == NULL)
         return NULL;
 
     for (int i = 0; i < string.len; i++)
-        result[i] = string.head[string.len - i];
+        result[i] = string.head[string.len - i - 1];
     
     return strIU7ng_create(result);    
 }
