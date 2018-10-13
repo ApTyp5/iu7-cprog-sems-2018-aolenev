92287cfac21d26c9f28fdb89a5e8eb6846641eb8
Pudov Dmitry <dimapudov99@gmail.com>
2018-10-13 15:48:10 +0300

    Documentation added

--- sem_03_strIU7ng/strIU7ng.h
+++ sem_03_strIU7ng/strIU7ng.h
@@ -30,10 +30,19 @@ Splits string by the delimiter
 */
 strIU7ng* split(strIU7ng string, char delimiter, int *new_size);
 
+/*
+Replace all occurences of replace at source
+*/
 strIU7ng* replace(strIU7ng source, strIU7ng replace);
 
+/*
+Returns repeated source multiply times
+*/
 strIU7ng mult(strIU7ng source, int multiply);
 
+/*
+Returns reversed string
+*/
 strIU7ng reverse(strIU7ng string);
 
 #endif
