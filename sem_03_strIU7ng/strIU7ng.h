1dcd6cbc924b5b0030677e6df5f2544ca40cf7ae
Pudov Dmitry <dimapudov99@gmail.com>
2018-10-13 15:37:19 +0300

    Signatures created

--- sem_03_strIU7ng/strIU7ng.h
+++ sem_03_strIU7ng/strIU7ng.h
@@ -7,8 +7,18 @@ struct strIU7ng
     char *head;
 }
 
-strIU7ng str_create(char *);
+strIU7ng strIU7ng_create(char *);
 
+void strIU7ng_free(strIU7ng string);
 
+int strIU7ng_len(strIU7ng string);
+
+strIU7ng* split(strIU7ng string, char delimiter);
+
+strIU7ng* replace(strIU7ng source, strIU7ng replace);
+
+strIU7ng mult(strIU7ng source, int multiply);
+
+strIU7ng reverse(strIU7ng string);
 
 #endif
