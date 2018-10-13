3d1d18ae0be44daa0877a28372098ad3ca9d12f3
kaa17u241 <13LEX04@gmail.com>
2018-10-13 19:59:38 +0300

    Added checks, bugs fixed

--- sem_03_strIU7ng/striu7ng.mult.c
+++ sem_03_strIU7ng/striu7ng.mult.c
@@ -5,14 +5,28 @@ strIU7ng mult(strIU7ng source, int multiply)
     strIU7ng new_str;
     int l_s = source.len;
 
-    new_str.len = l_s * multiply.
-    new_str.head = malloc(sizeof(char) * l_s * multiply);
-
-
-    for (int i = 0; i < multiply * l_s; i++)
+    if (multiply < 0)
     {
-        new_str.head[i] = source[l_s % i];
+        return ;
     }
+
+    new_str.len = l_s * multiply.
+    new_str.head = malloc(sizeof(char) * (new_str.len + 1));
+
+    if (new_str.head == NULL)
+    {
+        return ERR_ALLOCATION;
+    }
+    if (source.head == NULL)
+    {
+        return ERR_DEFAULT;
+    }
+
+    for (int i = 0; i < new_str.len; i++)
+    {
+        new_str.head[i] = source[i % l_s];
+    }
+    new_str.head[new_str.len] = 0;
 
     return new_str;
 }
