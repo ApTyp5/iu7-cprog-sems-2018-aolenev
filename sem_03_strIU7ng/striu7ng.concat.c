bf9bfec3fa4f2f3638bea7c97a952b844419a2a2
mdv17u911 <barefol@gmail.com>
2018-10-13 19:31:18 +0300

    Modify striu7ng_concat()

--- sem_03_strIU7ng/striu7ng.concat.c
+++ sem_03_strIU7ng/striu7ng.concat.c
@@ -1,15 +1,18 @@
-#inclide "striu7ng.h"
+#include "striu7ng.h"
 
 striu7ng striu7ng_concat(striu7ng str1, striu7ng str2)
 {
     int n = str1.len + str2.len;
     char str3[n + 1];
     
+    if (str1.head == NULL || str2.head == NULL)
+        return NULL;
+    
     for (int i = 0; i < str1.len; i++)
-        str3[i] = str1[i];
+        str3[i] = str1.head[i];
     
     for (int i = 0; i < str2.len; i++)
-        str3[str1 + i] = str2[i];
+        str3[str1.len + i] = str2.head[i];
     
     str3[n] = '\0';
     
