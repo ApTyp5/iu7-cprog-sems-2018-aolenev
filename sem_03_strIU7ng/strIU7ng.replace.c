e107ee27afea219ed7c50646ad57c6ec25d93798
nan17u724 <neknas28@gmail.com>
2018-10-13 19:58:45 +0300

    Added strIU7ng.replace.c

---
+++ sem_03_strIU7ng/strIU7ng.replace.c
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+#include "strIU7ng.h"
+
+strIU7ng replace(strIU7ng source, strIU7ng search, strIU7ng replace)
+{
+    char *find = NULL;
+    char *beg;
+    char *end;
+    int n;
+    int l;
+    char *new;
+
+    find = find_substrIU7ng(source, search);
+    
+    while (find != NULL)
+    {
+        beg = malloc((source.head - find) * sizeof(char));
+        
+        for (int i = 0; i < source.head - find; i++)
+        {
+            beg[i] = source.head[i];
+        }
+        
+        n = source.len - (source.head - find) - search.len;
+        end = malloc(n * sizeof(char));
+        
+        for (int i = 0; i < n; i++)
+        {
+            end[i] = (find + search.len)[i];
+        }
+        
+        l = source.len + (replace.len - search.len);
+        
+        new = malloc((l + 1) * sizeof(char));
+        
+        for (int i = 0; i < source.head - find; i++)
+        {
+            new[i] = beg[i];
+        }
+        
+        for (int i = 0; i < replace.len; i++)
+        {
+            new[i + source.head - find] = replace.head[i];
+        }
+        
+        for (int i = 0; i < n; i++)
+        {
+            new[i + source.head - find + replace.len] = end[i];
+        }
+        
+        new[l] = '\0';
+        
+        source = strIU7ng_create(new);
+        
+        find = find_substrIU7ng(source, search);
+    }
+    return source;
+}
