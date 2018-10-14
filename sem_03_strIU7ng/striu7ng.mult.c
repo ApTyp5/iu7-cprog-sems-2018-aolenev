6cd0c565ea148d4159ac762367170a6a84b5fd85
fnabiev <alsn_5@mail.ru>
2018-10-14 14:53:57 +0300

    add comments and documentation

--- sem_03_strIU7ng/striu7ng.mult.c
+++ sem_03_strIU7ng/striu7ng.mult.c
@@ -1,31 +1,30 @@
+#include <stdlib.h>
 #include "strIU7ng.h"
+
+/*!
+Создает строку new_str, содержащую multiply повторений подстроки source.
+
+@param source [in]
+@param multiply [in]
+
+@return Возвращает new_str в случае успешного выполнения,
+source - в случае ошибки.
+*/
 
 strIU7ng mult(strIU7ng source, int multiply)
 {
+    // проверка входных данных
+    if (source.len <= 0 || source.head == NULL || multiply <= 0)
+        return source;
+
+    // объявление и инициализация новой строки
     strIU7ng new_str;
-    int l_s = source.len;
+    new_str.len = source.len * multiply;
+    new_str.head = malloc(new_str.len * sizeof(char) + 1 * sizeof(char));
 
-    if (multiply < 0)
-    {
-        return ;
-    }
-
-    new_str.len = l_s * multiply.
-    new_str.head = malloc(sizeof(char) * (new_str.len + 1));
-
-    if (new_str.head == NULL)
-    {
-        return ERR_ALLOCATION;
-    }
-    if (source.head == NULL)
-    {
-        return ERR_DEFAULT;
-    }
-
+    // заполнение новой строки
     for (int i = 0; i < new_str.len; i++)
-    {
-        new_str.head[i] = source[i % l_s];
-    }
+        new_str.head[i] = source.head[i % source.len];
     new_str.head[new_str.len] = 0;
 
     return new_str;
