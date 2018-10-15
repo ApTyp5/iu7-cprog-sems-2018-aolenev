8d455464bf5bdb44e68da66fddbe5d007e6d7593
iim17u453 <ilyasov9999@gmail.com>
2018-10-15 15:19:50 +0300

    Doxygen documentation for free func was added.

--- sem_03_strIU7ng/strIU7ng.free.c
+++ sem_03_strIU7ng/strIU7ng.free.c
@@ -1,7 +1,11 @@
 #include <stdlib.h>
-
 #include "strIU7ng.h"
 
+/*
+Функция очищает память, выделенную под строку.
+
+@param *string [in]
+*/
 void strIU7ng_free(strIU7ng *string)
 {
     if (!string->head)
