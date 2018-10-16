a1a9ebcea8c80d75d5302eaea5167dccd4066543
iai17u666 <andrey3000.99@mail.ru>
2018-10-16 09:40:18 +0300

    add comments

--- sem_03_strIU7ng/strIU7ng.free.c
+++ sem_03_strIU7ng/strIU7ng.free.c
@@ -2,6 +2,11 @@
 
 #include "strIU7ng.h"
 
+/**
+ * @fn strIU7ng_free(strIU7ng *string)
+ * @brief Очищает строку и освобождает выделенную память
+ * @param string указатель на строку
+ */
 void strIU7ng_free(strIU7ng *string)
 {
     if (!string->head)
