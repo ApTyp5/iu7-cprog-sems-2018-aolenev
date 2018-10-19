adcfa1679750863bbcf10f74c11d998efda44395
Drozdov Vladimir <drozdovvd@student.bmstu.ru>
2018-10-19 16:01:02 +0300

    Added documentation for split function.

--- sem_03_strIU7ng/strIU7ng.split.c
+++ sem_03_strIU7ng/strIU7ng.split.c
@@ -2,6 +2,13 @@
 
 #include "strIU7ng.h"
 
+/**
+ * Replaces all occurrences of one character with another
+ * \param [in] str Source string
+ * \param [in] what Character that should be replaced
+ * \param [in] on_what Character with which previous character should be replaced
+ * \return Returns number of characters were replaced
+ */
 int byte_replace(strIU7ng str, char what, char on_what)
 {   
     int n = 0;
@@ -15,7 +22,13 @@ int byte_replace(strIU7ng str, char what, char on_what)
     return n;
 }
 
-
+/**
+ * Splits string by delimiter into an array of substrings
+ * \param [in] string Source string
+ * \param [in] delimiter Delimiter symbol
+ * \param [out] new_size Amount of strings source string was splitted into
+ * \return Returns array of substrings
+ */
 strIU7ng* strIU7ng_split(strIU7ng string, char delimiter, int *new_size)
 {
     int n = 1 + byte_replace(string, delimiter, 0);
