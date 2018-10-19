7ec235589711b7a00323939745077d52bebe98db
mdv17u911 <barefol@gmail.com>
2018-10-20 00:23:38 +0300

    Delete tabs

--- sem_03_strIU7ng/strIU7ng.concat.test.c
+++ sem_03_strIU7ng/strIU7ng.concat.test.c
@@ -7,13 +7,13 @@ void strIU7ng_concat_test_1(int *success)
     strIU7ng str2 = strIU7ng_create(" rules");
 
     strIU7ng str_res = strIU7ng_concat(str1, str2);
-	
+    
     if (str1.len + str2.len == str_res.len &&
-	    strcmp(str_res.head, "M&M rules") == 0)
-    	(*success)++;	
-	else
-		printf("'%s' + '%s'\nExpected: 'M&M rules'\nActual: '%s'",
-	        str1.head, str2.head, str_res.head);
+        strcmp(str_res.head, "M&M rules") == 0)
+        (*success)++;   
+    else
+        printf("'%s' + '%s'\nExpected: 'M&M rules'\nActual: '%s'",
+            str1.head, str2.head, str_res.head);
 }
 
 void strIU7ng_concat_test_2(int *success)
@@ -22,13 +22,13 @@ void strIU7ng_concat_test_2(int *success)
     strIU7ng str2 = strIU7ng_create("");
 
     strIU7ng str_res = strIU7ng_concat(str1, str2);
-	
+    
     if (str1.len + str2.len == str_res.len &&
-	    strcmp(str_res.head, "M&M") == 0)
-    	(*success)++;
-	else
-		printf("'%s' + '%s'\nExpected: 'M&M'\nActual: '%s'",
-	        str1.head, str2.head, str_res.head);		
+        strcmp(str_res.head, "M&M") == 0)
+        (*success)++;
+    else
+        printf("'%s' + '%s'\nExpected: 'M&M'\nActual: '%s'",
+            str1.head, str2.head, str_res.head);        
 }
 
 int main(void)
@@ -38,7 +38,7 @@ int main(void)
 
     strIU7ng_concat_test_1(&success);
     strIU7ng_concat_test_2(&success);
-	
+    
     printf("\n Function concat works at %d of %d tests\n", success, musttests);
 
     return 0;
