fb48113109db9ca6e0d247e7d99f824464efa43b
AptyPotatoch <aptyp_potatoch@mail.ru>
2018-10-27 19:01:52 +0300

    concat test improved

--- sem_04_liu7st/liu7st.tst.concat.c
+++ sem_04_liu7st/liu7st.tst.concat.c
@@ -1,6 +1,4 @@
-
 #include "tst.liu7st.concat.h"
-#include <stdio.h>
 #include "liu7st.h"
 
 
@@ -12,17 +10,10 @@ int main()
     return 0;
 }
 
-int byte_cmp(void *byte1, void *byte2, int len)
+int int_cmp(void *int1, void *int2)
 {
-    char *ch1 = byte1;
-    char *ch2 = byte2;
-
-    for (int i = 0; i < len; i++)
-        if (*ch1++ != *ch2++)
-            return NON_HAPPY_END;
-    return HAPPY_END;
+    return *int1 - int2;
 }
-
 
 void tst_1_liu7st_concat()
 {
@@ -48,7 +39,7 @@ void tst_1_liu7st_concat()
     liu7st ans = liu7st_concat(lst1, lst2);
     
     int res = HAPPY_END;
-    int exp_res = byte_cmp(ans->first.data, exp_ans->first.data, len * sizeof(liu7st);
+    int exp_res = compare(ans, exp_ans,) == 0 ? HAPPY_END : NON_HAPPY_END;
 
     PVERD(%d, res, exp_res);
 }
