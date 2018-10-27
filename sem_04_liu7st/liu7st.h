98bbd6a213e7e979e4ca95bdddef2454e7aee221
Stepanov Alexandr <happymainer@gmail.com>
2018-10-27 17:53:35 +0300

    EDIT: add comments in header file

--- sem_04_liu7st/liu7st.h
+++ sem_04_liu7st/liu7st.h
@@ -9,41 +9,41 @@ typedef struct
     void *data;
     liu7st *prev;
     liu7st *next;
-} liu7st_element
+} liu7st_element;
 
 typedef struct
 {
     int size;
     liu7st_element *first;
     liu7st_element *last;
-} liu7st
+} liu7st;
 
-// Sid
+// Sidenko
 liu7st liu7st_create();
 
-// Nabiev
+// Nabiev & Kyprii
 void liu7st_free(liu7st list);
 
-// Kosor
-void liu7st_append(truiu7st *list, striu7st_element striu7st_element;
+// Kosorykov & Coraev & 
+void liu7st_append(liu7st *list, void *data);
 
-// Moroz
-int liu7st_remove(striu7st *list, int i);
+// Moroz & Zlyh
+int liu7st_remove(liu7st *list, int i);
 
-// Nekl Konov
-int liu7st_insert(liu7st *list, liu7st_element element, int i);
+// Neklepaeva & Konovalova
+int liu7st_insert(liu7st *list, void *data, int i);
 
-// Potatoch Drozdov
+// Potapchyk & Drozdov
 lui7st liu7st_concat(liu7st first, liu7st second);
 
-// Pudov Grunk
+// Pudov & Grunkovsky
 int liu7st_has_cycle(liu7st list);
 
-// Gorb Chern
+// Gorbynov &  Chernenko
 lui7st liu7st_revert(liu7st list);
 
-// Ilyasov Ignatiev
+// Ilyasov & Ignatiev
 void liu7st_sort(liu7st *list);
 
-#ifndef
+#endif
 
