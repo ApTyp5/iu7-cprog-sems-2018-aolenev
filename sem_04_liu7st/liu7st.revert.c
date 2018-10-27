13b7fcda626dadf9c46d2a74c2158b4aa89612e4
Gorbunov <darkwen3@mail.ru>
2018-10-27 22:27:49 +0300

    add doxygen comments, fix errors and delete define

--- sem_04_liu7st/liu7st.revert.c
+++ sem_04_liu7st/liu7st.revert.c
@@ -1,23 +1,35 @@
 #include "liu7st.h"
 
-#define FREE_LIST 0
+/**
+ @file liu7st.revert.c
+ @brief Переворачивает список.
+ 
+ @param list [in] - структура с размером, указателями на первый и последний элементы списка
+ 
+ @return Возвращает структуру с размером, указателями на первый и последний элементы
+ перевернутого списка, если ошибок не было, иначе исходную структуру.
+*/
 
 liu7st liu7st_revert(liu7st list)
 {
+    // Проверка на пустой список
     if (list.size == FREE_LIST)
         return list;
     
+    // Проверка на пустые указатели
     if (list.first == NULL || list.last == NULL)
         return list;
     
     liu7st revert_list;
     liu7st_element *head;
-    liu7st_element *tmp;
+    void *tmp;
     
-    revert_list.size = list.size;
+    revert_list.size = 2;
     revert_list.first = list.last;
     revert_list.last = list.first;
     
+    // Меняются местами в первом и последнем элементах списка
+    // указатели на следующий и предыдущий элементы
     revert_list.first->next = revert_list.first->prev;
     revert_list.first->prev = NULL;
     
@@ -26,14 +38,21 @@ liu7st liu7st_revert(liu7st list)
     
     head = revert_list.first->next;
     
-    while (head != NULL)
+    // В каждом элементе списка меняются местами указатели
+    // на следующий и предыдущий элементы, считается размер списка
+    while (head->next != NULL)
     {
-        tmp->next = head->prev;
+        tmp = head->prev;
         head->prev = head->next;
-        head->next = tmp->next;
+        head->next = tmp;
         
         head = head->next;
+        ++(revert_list.size);
     }
+    
+    // Проверка полученного размера с ожидаемым
+    if (revert_list.size != list.size)
+        return list;
     
     return revert_list;
 }
