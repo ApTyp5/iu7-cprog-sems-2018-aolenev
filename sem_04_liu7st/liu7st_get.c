25019e1c0603e842247ee1de351607671adc6eec
geonerus <geonerus@mail.ru>
2018-10-27 18:40:50 +0300

    get added

---
+++ sem_04_liu7st/liu7st_get.c
@@ -0,0 +1,39 @@
+#include "liu7st.h"
+
+void *get(liu7st list, int i)
+{
+	if (i >= list.size)
+	{
+		return NULL;
+		
+	}
+	
+	if (i < 0)
+	{
+		i = list.size+i;
+		
+	}
+	
+    if(list.size - i < list.size / 2)
+	{
+		liu7st_element curr = *(list.first);
+		for(int x = 0; x != i; x++)
+		{
+			curr = *curr.next;
+		}
+		
+		return curr.data;		
+	}
+	else
+	{
+		liu7st_element curr = *(list.last);
+		for(int x = 0; x != i; x++)
+		{
+			curr = *curr.prev;
+		}
+	
+		return curr.data;				
+	}
+	
+	return NULL;
+}
