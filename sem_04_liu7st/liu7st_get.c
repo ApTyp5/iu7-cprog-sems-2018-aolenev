b234be8af1c3cdd5a40f8ebd0ca9a8597f8847bf
geonerus <geonerus@mail.ru>
2018-10-27 19:10:41 +0300

    codestyle fix

--- sem_04_liu7st/liu7st_get.c
+++ sem_04_liu7st/liu7st_get.c
@@ -1,39 +1,37 @@
 #include "liu7st.h"
+void set(liu7st list, int i, void *data)
 
-void *get(liu7st list, int i)
 {
-	if (i >= list.size)
-	{
-		return NULL;
-		
-	}
-	
-	if (i < 0)
-	{
-		i = list.size+i;
-		
-	}
-	
+    if (i >= list.size)
+    {
+        return NULL;	
+    }
+    if (i < 0)
+    {
+        i = list.size + i;
+    }    
     if(list.size - i < list.size / 2)
-	{
-		liu7st_element curr = *(list.first);
-		for(int x = 0; x != i; x++)
-		{
-			curr = *curr.next;
-		}
+    {
+        liu7st_element curr = *(list.first);
+
+        for(int x = 0; x != i; x++)
+        {
+            curr = *curr.next;
+        }
 		
-		return curr.data;		
+        return curr;	
 	}
-	else
-	{
-		liu7st_element curr = *(list.last);
-		for(int x = 0; x != i; x++)
-		{
-			curr = *curr.prev;
-		}
+    else
+    {
+        liu7st_element curr = *(list.last);
+   
+        for(int x = 0; x != i; x++)
+        {
+            curr = *curr.prev;
+        }
 	
-		return curr.data;				
-	}
-	
-	return NULL;
+        return curr;
+        }
+        
+        return NULL;
 }
