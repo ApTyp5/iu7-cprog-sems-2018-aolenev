a4a6459ef07ea747a76357f7b7f89b4eefe2eb9b
iai17u666 <andrey3000.99@mail.ru>
2018-10-27 18:42:14 +0300

    codestyle

--- sem_04_liu7st/liu7st.sort.c
+++ sem_04_liu7st/liu7st.sort.c
@@ -3,18 +3,20 @@
 void liu7st_sort(liu7st *list)
 {
     liu7st_element *elem = list->first;
-    
+
     for (int i = 0; i < list->size; i++)
     {
         for (int j = 0; j < list->size - i - 1; j++)
         {
             liu7st_element *nelem = elem->next;
+
             if (elem->data > nelem->data)
             {
                 if (elem == list->first)
                     list->first = nelem;
                 if (nelem == list->last)
                     list->last = elem;
+                
                 nelem->prev = elem->prev;
                 elem->next = nelem->next;
                 elem->prev = nelem;
