361148adc8c175b01b982d429f4e06886dad2428
geonerus <geonerus@mail.ru>
2018-10-27 19:02:32 +0300

    set added

---
+++ sem_04_liu7st/liu7st_set.c
@@ -0,0 +1,39 @@
+#include "liu7st.h"
+void set(liu7st list, int i, void *data)
+
+{
+    if (i >= list.size)
+    {
+        return -1;	
+    }
+    if (i < 0)
+    {
+        i = list.size + i;
+    }    
+    if(list.size - i < list.size / 2)
+    {
+        liu7st_element curr = *(list.first);
+
+        for(int x = 0; x != i; x++)
+        {
+            curr = *curr.next;
+        }
+		
+        curr.data = data;
+        return FREE_LIST;	
+	}
+    else
+    {
+        liu7st_element curr = *(list.last);
+   
+        for(int x = 0; x != i; x++)
+        {
+            curr = *curr.prev;
+        }
+	
+        curr.data = data;
+        return FREE_LIST;
+        }
+        
+        return -1;
+}
