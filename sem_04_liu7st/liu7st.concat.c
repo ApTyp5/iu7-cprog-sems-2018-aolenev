9d801513a183a7ec9c11b94aa3d53feb13f9fa4b
Drozdov Vladimir <drozdovvd@student.bmstu.ru>
2018-10-27 18:10:08 +0300

    Added liu7st_concat()

---
+++ sem_04_liu7st/liu7st.concat.c
@@ -0,0 +1,14 @@
+#include "liu7st.h"
+
+lui7st liu7st_concat(liu7st first, liu7st second)
+{
+    liu7st result = liu7st_create();
+
+    for (liu7st_element *el = first.first; el != NULL; el = el->next)
+        liu7st_append(&result, el->data);
+
+    for (liu7st_element *el = second.first; el != NULL; el = el->next)
+        liu7st_append(&result, el->data);
+
+    return result;
+}
