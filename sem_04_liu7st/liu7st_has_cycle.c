99b428597c5e417869320e65e547c2ee7eb83f1a
Dmitry Pudov <dimapudov99@gmail.com>
2018-10-27 18:10:13 +0300

    Current revision for Sergey

--- sem_04_liu7st/liu7st_has_cycle.c
+++ sem_04_liu7st/liu7st_has_cycle.c
@@ -3,8 +3,25 @@
 
 int liu7st_has_cycle(liu7st list)
 {
-    for (int i = 0; i < size; i++)
+    liu7st_element current_element;
+    if (list.size <= 0)
     {
+        return 0;
+    }
+
+    if (list.first == NULL || list.last == NULL)
+    {
+        return 0;
+    }
+
+    current_element = *(list.head);
+    for (int i = 0; i < list.size; i++)
+    {
+        if (current_element.next == NULL)
+        {
+            return 0;
+        }
+        current_element = *(current_element.next);
 
     }
 }
