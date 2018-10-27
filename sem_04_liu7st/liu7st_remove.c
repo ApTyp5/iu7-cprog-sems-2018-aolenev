7eb6669b5f6407d85c048b7f5eafd74c2c0163ef
mdv17u911 <barefol@gmail.com>
2018-10-27 18:54:39 +0300

    Add doxygen comments and fix codestyle

--- sem_04_liu7st/liu7st_remove.c
+++ sem_04_liu7st/liu7st_remove.c
@@ -1,31 +1,40 @@
+/**
+ * \file liu7st_remove.c
+ * \brief Содержит функцию liu7st_remove()
+ */
+
 #include "liu7st_remove.h"
 #include "liu7st.h"
 
+/**
+ * \fn int liu7st_remove(striu7st *list, int i)
+ * \brief Удаляет элемент списка 
+ * \param [in] list Указатель на список
+ * \param [in] i Индекс удаляемого элемента 
+ * \return Код выполнения
+ */
 int liu7st_remove(striu7st *list, int i)
 {
+    liu7st_element *p = list->first; //< Указатель на удаляемый элемент
+    
     if (i >= list->size || i < 0)
         return WRONG_INDEX;
     
-    liu7st_element *p = list->first;
-    
     for (int a = 0; a < i; a++)
-    {
         p = p->next;
-    }
-    
-    free(p->data);
     
     if (i)
         (p->prev)->next = p->next;
     else
         list->begin = p->next;
    
-
-    if (p != list->end)
+    if (p != list->size - 1)
         (p->next)->prev = p->prev;
     else
         list->end = p->prev;
     
+    free(p->data);
+    free(p);
     (list->size)--;
     
     return SUCCESS;
