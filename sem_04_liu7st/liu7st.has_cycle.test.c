78a4e12ea9d6b47379d756425eb3ca703e6ed9d2
gsi17u168 <sergrun@mail.ru>
2018-10-27 18:58:37 +0300

    All tests

--- sem_04_liu7st/liu7st.has_cycle.test.c
+++ sem_04_liu7st/liu7st.has_cycle.test.c
@@ -17,23 +17,58 @@ void test_no_cycle(int *passed)
     int expected = 0;
     liu7st list;
     
+    list.size = 3;
+    list.first = malloc(sizeof(liu7st_element));
+    list.first->next = malloc(sizeof(liu7st_element));
+    list.first->prev = NULL;
+    list.first->next->next = malloc(sizeof(liu7st_element));
+    list.first->next->prev = list.first->next;
+    list.first->next->next->next = NULL;
+    list.first->next->next->prev = list.first->next->next->prev;
+    
+    if (liu7st_has_cycle(list) == expected)
+    {
+        *passed = *passed + 1;
+    }
+    
+    free(list.first->next->next);
+    free(list.first->next);
+    free(list.first);
+}
+
+void test_circular(int *passed)
+{
+    int expected = 1;
+    liu7st list;
     
     list.size = 3;
     list.first = malloc(sizeof(liu7st_element));
-    (*liu7st_element)list.first.next = malloc(sizeof(liu7st_element));
-    list.first.prev = NULL;
-    list.first.next.next = malloc(sizeof(liu7st_element));
-    list.first.next.prev = list.first.next;
-    list.first.next.next.next = NULL;
-    list.first.next.next.prev = list.first.next.next.prev;
+    list.first->next = malloc(sizeof(liu7st_element));
+    list.first->prev = NULL;
+    list.first->next->next = malloc(sizeof(liu7st_element));
+    list.first->next->prev = list.first->next;
+    list.first->next->next->next = list.first;
+    list.first->next->next->prev = list.first->next->next->prev;
+    
+    if (liu7st_has_cycle(list) == expected)
+    {
+        *passed = *passed + 1;
+    }
+    
+    free(list.first->next->next);
+    free(list.first->next);
+    free(list.first);
 }
+
+void 
 
 int main()
 {
     int passed = 0;
     
-    
-    
+    test_empty(&passed);
+    test_no_cycle(&passed);
+    test_circular(&passed);
     
     
     return 0;
