3736c3bf087b83decee1021762d49d1b0c5c3ed1
Анастасия <anastasia@MacBook-Pro-Anastasia.local>
2018-10-27 18:26:54 +0300

    Delete malloc

--- sem_04_liu7st/liu7st.create.c
+++ sem_04_liu7st/liu7st.create.c
@@ -4,9 +4,7 @@ liu7st create()
 {
     liu7st list;
     list.size = 0;
-    list.first = malloc(sizeof(liu7st_element *));
     list.first = NULL;
-    list.last = malloc(sizeof(liu7st_element *));
     list.last = NULL;
 
     return list; 
