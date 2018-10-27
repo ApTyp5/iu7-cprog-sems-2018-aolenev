ba429d1887ad50a39193ced6091bf40c2c884716
Dmitry Pudov <dimapudov99@gmail.com>
2018-10-27 18:31:39 +0300

    Raw edition of has_cycle written

--- sem_04_liu7st/liu7st_has_cycle.c
+++ sem_04_liu7st/liu7st_has_cycle.c
@@ -3,7 +3,6 @@
 
 int liu7st_has_cycle(liu7st list)
 {
-    liu7st_element current_element;
     if (list.size <= 0)
     {
         return 0;
@@ -14,14 +13,36 @@ int liu7st_has_cycle(liu7st list)
         return 0;
     }
 
-    current_element = *(list.head);
-    for (int i = 0; i < list.size; i++)
+    if (list.last->next == list.first || list.first->prev = list.last)
     {
-        if (current_element.next == NULL)
-        {
-            return 0;
-        }
-        current_element = *(current_element.next);
-
+        return 1;
     }
+
+    liu7st_element current_element = *(list.first);
+    int counter = 0;
+    while (current_element->next != NULL || counter <= list.size + 1)
+    {
+        counter++;
+    }
+
+    if (current_element->next == NULL && counter < list.size - 1)
+    {
+        return 0;
+    }
+
+    // if (counter > list.size + 1)
+    // {
+    //     int it_1 = 0, it_2 = 0;
+    //
+    //     while (it_1 <= it_2 || it_1 )
+    //     {
+    //         it_1 += 1;
+    //         it_2 += 2;
+    //
+    //     }
+    //
+    //     return 1;
+    // }
+
+    return 0;
 }
