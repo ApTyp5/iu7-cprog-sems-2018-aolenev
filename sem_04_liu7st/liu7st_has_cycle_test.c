b1c03002f4361d807d05c243a4b7cd134e2f0f88
gsi17u168 <sergrun@mail.ru>
2018-10-27 18:43:05 +0300

    Initial commit;

---
+++ sem_04_liu7st/liu7st_has_cycle_test.c
@@ -0,0 +1,40 @@
+#include "liu7st.h"
+#include <stdio.h>
+
+void test_empty(int *passed)
+{
+    int expected = 0;
+    liu7st list;
+    
+    if (liu7st_has_cycle(list) == expected)
+    {
+        *passed = *passed + 1;
+    }
+}
+
+void test_no_cycle(int *passed)
+{
+    int expected = 0;
+    liu7st list;
+    
+    
+    list.size = 3;
+    list.first = malloc(sizeof(liu7st_element));
+    (*liu7st_element)list.first.next = malloc(sizeof(liu7st_element));
+    list.first.prev = NULL;
+    list.first.next.next = malloc(sizeof(liu7st_element));
+    list.first.next.prev = list.first.next;
+    list.first.next.next.next = NULL;
+    list.first.next.next.prev = list.first.next.next.prev;
+}
+
+int main()
+{
+    int passed = 0;
+    
+    
+    
+    
+    
+    return 0;
+}
