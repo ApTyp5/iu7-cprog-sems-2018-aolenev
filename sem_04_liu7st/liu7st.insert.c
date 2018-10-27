c12edd317a613232f6eeb232a50779edd1c352d7
nan17u724 <neknas28@gmail.com>
2018-10-27 18:55:46 +0300

    insert.c added

---
+++ sem_04_liu7st/liu7st.insert.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "liu7st.h"
+
+int liu7st_insert(liu7st *list, void *data, int i)
+{
+   int count = 0;
+   liu7st_element *sup = list->first;
+   liu7st_element *node = malloc(sizeof(liu7st_element));
+   node->data = data;
+   
+   while (count != i - 1)
+   {
+       count++;
+       sup = sup->next;    
+   }
+   
+   node->prev = sup;
+   sup = sup->next;
+   node->prev = sup;
+   sup->prev = node;
+   sup = sup->prev;
+   sup = sup->prev;
+   sup->next = node;
+   
+   list->size += 1;
+   
+   return SUCCESS;
+}
