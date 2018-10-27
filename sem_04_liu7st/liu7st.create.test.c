12368f4c5af7e88fdfb2f7f3104b0779d2316f1e
sag17u762 <nasty.sidenko@mail.ru>
2018-10-27 23:01:14 +0300

    Codestyle

--- sem_04_liu7st/liu7st.create.test.c
+++ sem_04_liu7st/liu7st.create.test.c
@@ -3,11 +3,13 @@
 int main()
 {
     liu7st list = liu7st_create();
+    
     if (NULL == list.first && NULL == list.last && 0 == list.size)
     {
 	printf("SUCCESS");
 	return SUCCESS;
     }
+
     printf("FAILED");
     return FAILED;
 } 
