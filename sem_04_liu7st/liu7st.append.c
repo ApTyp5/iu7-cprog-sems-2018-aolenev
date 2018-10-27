7bae4e31f5d0a1c7cbe27f3b795e5396d40c40d7
Drozdov Vladimir <drozdovvd@student.bmstu.ru>
2018-10-27 18:44:07 +0300

    Added doxygen comment for liu7st_append()

--- sem_04_liu7st/liu7st.append.c
+++ sem_04_liu7st/liu7st.append.c
@@ -1,5 +1,10 @@
 #include "liu7st.h"
 
+/**
+ * Appends element to the end of a list
+ * \param [in] list
+ * \param [in] data
+ */
 void liu7st_append(liu7st *list, void *data)
 {
     liu7st_insert(list, data, list->size);
