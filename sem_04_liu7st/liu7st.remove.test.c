6c832ecf0b6057b73e47417dd2c4920aee5f6c72
mdv17u911 <barefol@gmail.com>
2018-10-29 19:00:09 +0300

    Add unit tests for liu7st_remove()

---
+++ sem_04_liu7st/liu7st.remove.test.c
@@ -0,0 +1,66 @@
+#include "liu7st.h"
+
+void liu7st_remove_test_1(int *success)
+{
+	int status;
+	int a[3] = {2, 3, 4};
+    liu7st list = liu7st_create();
+
+	
+    liu7st_append(&list, a);
+	liu7st_append(&list, a + 1);
+    liu7st_append(&list, a + 1);
+    
+    status = liu7st_remove(&list, 1);
+	
+	if (list.size == 2 && *(int *)((list.first)->data) == 2 &&
+	    status == SUCCESS && *(int *)(((list.first)->next)->data) == 4)
+		(*success)++;
+	else
+		printf("\nTest 1:\nexpected: n = 2\nActual: n = %d\n", list.size);
+	
+	status = liu7st_remove(&list, 0);
+	
+	if (list.size == 1 && *(int *)((list.first)->data) == 4
+	    && status == SUCCESS)
+		(*success)++;
+	else
+		printf("\nTest 2:\nexpected: n = 1\nActual: n = %d\n", list.size);
+	
+	status = liu7st_remove(&list, 0);
+	
+	if (list.size == 0 && status == SUCCESS)
+		(*success)++;
+	else
+		printf("\nTest 3:\nexpected: n = 0\nActual: n = %d\n", list.size);
+	
+	status = liu7st_remove(&list, 0);
+	
+	if (list.size == 0 && status == WRONG_INDEX)
+		(*success)++;
+	else
+		printf("\nTest 4:\nexpected: Wrong index\nActual: %d\n", status);
+
+    status = liu7st_remove(NULL, 0);
+	
+	if (status == WRONG_LIST)
+		(*success)++;
+	else
+		printf("\nTest 5:\nexpected: Wrong list\nActual: %d\n", status);	
+}
+
+int main(void)
+{
+    int success = 0;
+    int musttests = 5;
+
+    liu7st_remove_test_1(&success);
+    //liu7st_remove_test_2(&success);
+    //liu7st_remove_test_3(&success);
+    //liu7st_remove_test_4(&success);
+    //liu7st_remove_test_5(&success);
+    
+    printf("\n Function liu7st_remove() works at %d of %d tests\n", success, musttests);
+
+    return SUCCESS;
+}
