0468b5087f37cb4ca413f6dd31ec0066f01e727a
nan17u724 <neknas28@gmail.com>
2018-10-30 19:45:23 +0300

    Check in liu7st_insert added

--- sem_04_liu7st/liu7st.insert.c
+++ sem_04_liu7st/liu7st.insert.c
@@ -17,7 +17,7 @@ int liu7st_insert(liu7st *list, void *data, int i)
    if ((i < 0) || (i > list->size))
        return FAILED;
    
-   if ((list->first == NULL) || (list->last == NULL))
+   if ((list->first == NULL) || (list->last == NULL) || (list == NULL))
        return FAILED;
    
    int count = 0;
