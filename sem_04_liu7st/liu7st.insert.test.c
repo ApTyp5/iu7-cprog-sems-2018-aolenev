de4335d075faba53f0dc3a899aab54171ddaa14b
kos17u844 <jeni62@mail.ru>
2018-10-30 21:29:00 +0300

    Fixed inserts tests

--- sem_04_liu7st/liu7st.insert.test.c
+++ sem_04_liu7st/liu7st.insert.test.c
@@ -2,20 +2,20 @@
 
 void insert_test1(int *success_tests)
 {
-    liu7st list = create();
+    liu7st list = liu7st_create();
     int success = 0;
     int data;
     int size = 5;
     for (int i = 0; i < size; i++)
     {
         data = i;
-        liu7st.insert(&list, &data, i);
+        liu7st_insert(&list, &data, i);
     }
     void *pointer;
     
     for (int i = 0; i < size; i++)
     {
-        get(&list, i, &pointer);
+        liu7st_get(&list, i, &pointer);
         if (*((int*)pointer) = i)
             success++;
     }
@@ -26,10 +26,10 @@ void insert_test1(int *success_tests)
 
 void insert_test2(int *success_tests)
 {
-    liu7st list = create();
+    liu7st list = liu7st_create();
     int i = -1;
     int data = 2;
-    int code_mistake = liu7st.insert(&list, &data, i);
+    int code_mistake = liu7st_insert(&list, &data, i);
     
     if ((code_mistake != 0) && (list->size == 0))
         (*success_tests)++;
@@ -37,10 +37,10 @@ void insert_test2(int *success_tests)
 
 void insert_test3(int *success_tests)
 {
-    liu7st list = create();
+    liu7st list = liu7st_create();
     int i = 1;
     int data = 2;
-    int code_mistake = liu7st.insert(&list, &data, i);
+    int code_mistake = liu7st_insert(&list, &data, i);
     
     if ((code_mistake != 0) && (list->size == 0))
         (*success_tests)++;
@@ -48,17 +48,17 @@ void insert_test3(int *success_tests)
 
 void insert_test4(int *success_tests)
 {
-    liu7st list = create();
+    liu7st list = liu7st_create();
     int success = 0;
     float data[] = { 1.5, 5.4, -2.3, 0.8, 9.7 };
     int size = 5;
     for (int i = 0; i < size; i++)
-        liu7st.insert(&list, &data[i], i);
+        liu7st_insert(&list, &data[i], i);
     void *pointer;
     
     for (int i = 0; i < size; i++)
     {
-        get(&list, i, &pointer);
+        liu7st_get(&list, i, &pointer);
         if (*((float*)pointer) = data[i])
             success++;
     }
