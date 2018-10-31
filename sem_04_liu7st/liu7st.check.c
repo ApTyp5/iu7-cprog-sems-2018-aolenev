b0feb7c8023edd6191c9242ec60925b26029526c
Drozdov Vladimir <drozdovvd@student.bmstu.ru>
2018-10-31 20:16:35 +0300

    Added initial version of liu7st_check()

---
+++ sem_04_liu7st/liu7st.check.c
@@ -0,0 +1,36 @@
+#include "liu7st.h"
+
+int liu7st_check(liu7st list)
+{
+    if ((list.first == NULL) ^ (list.last == NULL))
+        return WRONG_LIST;
+
+    if (list.first == NULL && list.last == NULL)
+        return list.size == 0 ? SUCCESS : WRONG_LIST;
+
+    if (list.first->prev != NULL || list.last->next != NULL)
+        return WRONG_LIST;
+
+    liu7st_element *p1 = list.first, *p2 = p1;
+    int size = 1;
+
+    do
+    {
+        p1 = p1->next;
+        
+        for (int i = 0; i < 2; i++)
+        {
+            if (p2->next != NULL && p2->next->prev != p2)
+                return WRONG_LIST;
+
+            if (p2->next == NULL)
+                return list.last == p2 && list.size == size ? SUCCESS : WRONG_LIST;
+
+            p2 = p2->next;
+            size++;
+        }
+    } 
+    while (p1 != p2);
+
+    return WRONG_LIST;
+}
