f5657d38f71511e8255349b3668262f8bd3c84c2
Drozdov Vladimir <drozdovvd@student.bmstu.ru>
2018-11-02 00:52:25 +0300

    Added documentation for iterator functions

--- sem_04_liu7st/liu7st.iterator.c
+++ sem_04_liu7st/liu7st.iterator.c
@@ -2,14 +2,26 @@
 
 #define OFFSET(STRUCT, MEMBER) (size_t)(&((STRUCT*)0)->MEMBER)
 
+/**
+ * Creates iterator pointing to the first list entry
+ * \param [in] list
+ * \retval NULL If list pointer is null
+ * \retval iterator Otherwise
+ */
 void *liu7st_begin(liu7st *list)
 {
     if (list == NULL)
         return NULL;
-    
+
     return &list->first->data;
 }
 
+/**
+ * Creates iterator pointing to the last list entry
+ * \param [in] list
+ * \retval NULL If list pointer is null
+ * \retval iterator Otherwise
+ */
 void *liu7st_end(liu7st *list)
 {
     if (list == NULL)
@@ -18,11 +30,17 @@ void *liu7st_end(liu7st *list)
     return &list->last->data;
 }
 
+/**
+ * Moves iterator to the next list entry
+ * \param [in] iterator
+ * \retval NULL If iterator was already null or if there is no next entry
+ * \retval new_iterator Otherwise
+ */
 void *liu7st_next(void *iterator)
 {
     if (iterator == NULL)
         return NULL;
-    
+
     liu7st_element *p = (liu7st_element*)((char*)iterator - OFFSET(liu7st_element, data));
 
     if (p->next == NULL)
@@ -31,11 +49,17 @@ void *liu7st_next(void *iterator)
     return &p->next->data;
 }
 
+/**
+ * Moves iterator to the previous list entry
+ * \param [in] iterator
+ * \retval NULL If iterator was already null or if there is no previous entry
+ * \retval new_iterator Otherwise
+ */
 void *liu7st_prev(void *iterator)
 {
     if (iterator == NULL)
         return NULL;
-    
+
     liu7st_element *p = (liu7st_element*)((char*)iterator - OFFSET(liu7st_element, data));
 
     if (p->prev == NULL)
