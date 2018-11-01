6044857936742b8cf77077c9301c88c37bb6751c
gsi17u168 <sergrun@mail.ru>
2018-11-01 10:28:58 +0300

    issue complete, git-master loh

--- sem_04_liu7st/liu7st.has_cycle.test.c
+++ sem_04_liu7st/liu7st.has_cycle.test.c
@@ -6,9 +6,17 @@ void test_empty(int *passed)
     int expected = 0;
     liu7st list;
     
+    list.size = 0;
+    list.first = NULL;
+    list.last = NULL;
+
     if (liu7st_has_cycle(list) == expected)
     {
         *passed = *passed + 1;
+    }
+    else
+    {
+	printf("Empty test not passed\n");
     }
 }
 
@@ -30,7 +38,10 @@ void test_no_cycle(int *passed)
     {
         *passed = *passed + 1;
     }
-    
+    else
+    {
+	printf("No cycle test not passed\n");    
+    }
     free(list.first->next->next);
     free(list.first->next);
     free(list.first);
@@ -53,6 +64,10 @@ void test_circular(int *passed)
     if (liu7st_has_cycle(list) == expected)
     {
         *passed = *passed + 1;
+    }
+    else
+    {
+	printf("Circular test not passed\n");
     }
     
     free(list.first->next->next);
