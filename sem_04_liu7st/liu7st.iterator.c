cce3a5e52007544bb0f6fdb78314f09ccc5f69cd
Drozdov Vladimir <drozdovvd@student.bmstu.ru>
2018-11-01 16:59:53 +0300

    Added initial version of list iterators

---
+++ sem_04_liu7st/liu7st.iterator.c
@@ -0,0 +1,43 @@
+#include "liu7st.h"
+
+#define OFFSET(STRUCT, MEMBER) (size_t)(&((STRUCT*)NULL)->MEMBER)
+
+void *liu7st_begin(liu7st *list)
+{
+    if (list == NULL)
+        return NULL;
+    return &list->first->data;;
+}
+
+void *liu7st_end(liu7st *list)
+{
+    if (list == NULL)
+        return NULL;
+    return &list->last->data;;
+}
+
+void *liu7st_next(void *iterator)
+{
+    if (iterator == NULL)
+        return NULL;
+    
+    liu7st_element *p = (liu7st_element*)((char*)iterator - OFFSET(liu7st_element, data));
+
+    if (p->next == NULL)
+        return NULL;
+
+    return &p->next->data;
+}
+
+void *liu7st_prev(void *iterator)
+{
+    if (iterator == NULL)
+        return NULL;
+    
+    liu7st_element *p = (liu7st_element*)((char*)iterator - OFFSET(liu7st_element, data));
+
+    if (p->prev == NULL)
+        return NULL;
+
+    return &p->prev->data;
+}
