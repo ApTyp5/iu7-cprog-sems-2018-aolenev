57d3ca76a71565657b7b3c154f8eb0fc99b85230
iai17u666 <andrey3000.99@mail.ru>
2018-11-01 08:30:20 +0300

    fix not updating prev and next of compared pair

--- sem_04_liu7st/liu7st.sort.c
+++ sem_04_liu7st/liu7st.sort.c
@@ -13,7 +13,7 @@ void liu7st_sort(liu7st *list, int (*comp)(const void*, const void*))
 
     for (int i = 0; i < list->size; i++)
     {
-        for (int j = 0; j < list->size - i - 2; j++)
+        for (int j = 0; j < list->size - i - 1; j++)
         {
             liu7st_element *nelem = elem->next;
 
@@ -21,9 +21,13 @@ void liu7st_sort(liu7st *list, int (*comp)(const void*, const void*))
             {
                 if (elem == list->first)
                     list->first = nelem;
+                else
+                    elem->prev->next = nelem;
 
                 if (nelem == list->last)
                     list->last = elem;
+                else
+                    nelem->next->prev = elem;
 
                 nelem->prev = elem->prev;
                 elem->next = nelem->next;
