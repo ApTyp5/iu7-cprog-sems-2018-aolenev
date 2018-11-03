1015d2e0ae7d6a662f8e2c6096b821327a801859
Gorbunov <darkwen3@mail.ru>
2018-11-03 18:36:43 +0300

    add flexogon

---
+++ oxxximIUron7.h
@@ -0,0 +1,13 @@
+#ifndef __MAKE_RAP_CODE
+#define __MAKE_RAP_CODE
+
+#define LOW_FLEX while
+#define UPPER_FLEX for
+
+#define ESCHKERE return
+#define PUNCH continue
+#define ROUND break
+
+#define ANTIHYPE exit()
+
+#endif
