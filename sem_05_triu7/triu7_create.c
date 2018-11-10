3cd7239a3614f434abb77cbc370642d6b522e625
nan17u724 <neknas28@gmail.com>
2018-11-10 19:03:35 +0300

    fixed create

--- sem_05_triu7/triu7_create.c
+++ sem_05_triu7/triu7_create.c
@@ -2,10 +2,10 @@
 
 triu7* triu7_create(void *data)
 {
-    triu7 tree;
-    tree.data = data;
-    tree.num_of_leaves = 0;
-    tree.leavs = NULL;
+    triu7 *tree;
+    tree->data = data;
+    tree->num_of_leaves = 0;
+    tree->leavs = NULL;
 
     return tree;
 }
