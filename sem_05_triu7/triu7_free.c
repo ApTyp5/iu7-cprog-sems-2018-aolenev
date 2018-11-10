3489647078d9ab301dad2d88381cc6cce0365ef8
iim17u453 <ilyasov9999@gmail.com>
2018-11-10 18:43:14 +0300

    First version.

--- sem_05_triu7/triu7_free.c
+++ sem_05_triu7/triu7_free.c
@@ -1,6 +1,24 @@
 #include "triu7.h"
+#include <stdio.h>
 
 void triu7_free(triu7 *tree)
 {
-    return;
+    if (tree->data == NULL || tree->num_of_leaves == 0 || tree->leavs == NULL)
+    {
+        return;
+    }
+    
+    for (int i = 0; i < tree->num_of_leaves; i++)
+    {
+        tree->data = NULL;
+        tree->leavs = NULL;
+    }
+    
+    tree->num_of_leaves = 0;
+}
+
+int main(void)
+{
+    printf("Kek\n");
+    return SUCCESS;
 }
