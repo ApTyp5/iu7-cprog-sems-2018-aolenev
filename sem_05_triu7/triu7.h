4d748b2899f67fb03078e694d104c1b258996033
R2r <aptyp.potatoch@gmail.com>
2018-11-10 18:40:33 +0300

    header improved

--- sem_05_triu7/triu7.h
+++ sem_05_triu7/triu7.h
@@ -28,16 +28,16 @@ triu7* triu7_create(void *data);
 
 // Пудов, Грунковский
 
-int triu7_has_cycle(triu7 tree);
+int triu7_has_cycle(triu7 *tree);
 
 // Дроздов, Степанов
 int triu7_apply(triu7 *tree_head, void func(void *data));
 
 // Мороз, Москаленко
-int triu7_size(triu7 tree);
+int triu7_size(triu7 *tree);
 
 // Горбунов
-int triu7_depth(triu7 tree);
+int triu7_depth(triu7 *tree);
 
 #endif
 
