3a7f551aa9ff0bbdb502347d118c081a090bbdf0
Dmitry Pudov <dimapudov99@gmail.com>
2018-11-10 18:48:18 +0300

    contains function

--- sem_05_triu7/triu7.has_cycle.c
+++ sem_05_triu7/triu7.has_cycle.c
@@ -1,1 +1,32 @@
 #include "triu7.h"
+#define TRUE 1
+#define FALSE 0
+
+typedef struct {
+    triu7 **pointers;
+    int count;
+} PointersSet;
+
+int contains(PointersSet *set, triu7 *pointer)
+{
+    if (set == NULL || pointer == NULL)
+    {
+        return FALSE;
+    }
+
+    for (int i = 0; i < set->count; i++)
+    {
+        if (*(set->pointers + i) == pointer)
+        {
+            return TRUE;
+        }
+    }
+
+    return FALSE;
+}
+
+int has_cycle(triu7 *tree)
+{
+    
+    return FALSE;
+}
