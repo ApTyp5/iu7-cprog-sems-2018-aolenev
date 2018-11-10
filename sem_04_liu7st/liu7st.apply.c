2a18a4736256d957a81fc089edc347bef4cbc58f
Stepanov Alexandr <happymainer@gmail.com>
2018-11-10 12:40:47 +0300

    EDIT: add comments for apply

--- sem_04_liu7st/liu7st.apply.c
+++ sem_04_liu7st/liu7st.apply.c
@@ -1,5 +1,15 @@
 #include "liu7st.h"
 
+/*
+ * \brief Функция выполняет action над всеми элементами, начиная с номера index
+ *
+ * \param [out] list - список
+ * \param [in]  index - номер элемента, с которого начать
+ * \param [in]  action - действие для элементов
+ *
+ * \return Возвращает SUCCESS при успешнов выполнении, WRONG_LIST при некорректном списке и
+ * WRONG_INDEX при некорректном индексе
+ */
 int liu7st_apply(liu7st list, int index, void (*action)(void*))
 {
     if (list.first == NULL || list.last == NULL)
