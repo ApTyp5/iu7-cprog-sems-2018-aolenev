e241007ee5b7f698ca5292c8ad8b544e937f38ca
nan17u724 <neknas28@gmail.com>
2018-11-10 18:54:49 +0300

    Fixed create

--- sem_05_triu7/triu7_create.c
+++ sem_05_triu7/triu7_create.c
@@ -1,10 +1,10 @@
 #include "triu7.h"
 
-triu7 triu7_create(void *data)
+triu7* triu7_create(void *data)
 {
     triu7 tree;
     tree.data = data;
-    tree.num_of_leaves = 1;
+    tree.num_of_leaves = 0;
     tree.leavs = NULL;
 
     return tree;
