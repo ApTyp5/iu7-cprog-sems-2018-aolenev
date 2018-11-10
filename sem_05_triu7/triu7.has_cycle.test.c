fb7057a3899ccd4ae07f9cc517b016a82b09639b
gsi17u168 <sergrun@mail.ru>
2018-11-10 18:27:24 +0300

    placeholders for tests

---
+++ sem_05_triu7/triu7.has_cycle.test.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+#include "triu7.h"
+
+void test_no_cycle(int *passed)
+{
+    int expected = FALSE;
+}
+
+void test_cycle_1(int *passed)
+{
+    int expected = TRUE;
+}
+
+void test_cycle_2(int *passed)
+{
+    int expected = TRUE;
+}
+
+void test_cycle_3(int *passed)
+{
+    int expected = TRUE;
+}
+
+void test_invalid_1(int *passed)
+{
+    int expected = FALSE;
+}
+
+void test_invalid_2(int *passed)
+{
+    int expected = FALSE;
+}
+
+int main()
+{
+    int passed = 0;
+    
+    
+    
+    return SUCCESS;
+}
