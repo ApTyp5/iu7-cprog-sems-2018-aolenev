938b398eb93d4a3b7ae97809fa4f1c1b3413574a
Stepanov Alexandr <happymainer@gmail.com>
2018-11-10 18:29:19 +0300

    EDIT: add pointers

--- sem_05_triu7/triu7.apply.c
+++ sem_05_triu7/triu7.apply.c
@@ -1,15 +1,18 @@
 #include "triu7.h"
 
-int triu7_apply(triu7 tree_head, void func(void*))
+int triu7_apply(triu7 *tree_head, void func(void*))
 {
+    if (tree_head == NULL)
+        return WRONG_TREE;
+
     if (func == NULL)
         return WRONG_FUNCTION;
 
-    func(tree_head.data);
+    func(tree_head->data);
 
     for (int i = 0; i < num_of_leaves; i++)
     {
-        triu7_apply(tree_head.leavs[i], func);
+        triu7_apply(tree_head->leavs[i], func);
     }
 
     return SUCCESS;
