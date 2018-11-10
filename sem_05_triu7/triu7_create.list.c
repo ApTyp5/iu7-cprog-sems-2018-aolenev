a4545eb5e5d141b042ddbee8b6f493443dc105af
kos17u844 <jeni62@mail.ru>
2018-11-10 18:49:37 +0300

    Fixed mistake in create_test

--- sem_05_triu7/triu7_create.list.c
+++ sem_05_triu7/triu7_create.list.c
@@ -6,7 +6,7 @@ int main()
     void *data = &x;
     triu7 tree = triu7_create(data);
     
-    if (triu7.data == data && triu7.num_of_leaves == 1 && triu7.leavs == NULL)
+    if (triu7.data == data && triu7.num_of_leaves == 0 && triu7.leavs == NULL)
         return SUCCESS;
     
     return FAILED;
