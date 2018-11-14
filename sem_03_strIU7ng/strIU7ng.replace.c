0cd4996bf81404ccaea0abd125e531a3652dc97e
Gorbunov <darkwen3@mail.ru>
2018-11-15 00:32:12 +0300

    add func compare_str

--- sem_03_strIU7ng/strIU7ng.replace.c
+++ sem_03_strIU7ng/strIU7ng.replace.c
@@ -1,7 +1,38 @@
-//#include <string.h>
+/**
+ @file strIU7ng.replace.c
+ */
+
 #include "strIU7ng.h"
 
 /**
+ @fn int my_strcmp(const char *str_1, const char *str_2)
+ @brief Проверяет одинаковые ли строки
+ 
+ @param str_1 [in] - указатель на первую строку
+ @param str_2 [in] - указатель на вторую строку
+ 
+ @return Возвращает SUCCESS, если строки равны, иначе ERR_DEFAULT
+ */
+
+int compare_str(const char *str_1, const char *str_2)
+{
+    int size_1, size_2;
+    
+    for (size_1 = 0; str_1[size_1] != '\0'; size_1++);
+    for (size_2 = 0; str_2[size_2] != '\0'; size_2++);
+    
+    if (size_1 != size_2)
+        return ERR_DEFAULT;
+    
+    for (int i = 0; i < size_1; i++)
+        if (str_1[i] != str_2[i])
+            return ERR_DEFAULT;
+    
+    return SUCCESS;
+}
+
+/**
+ @fn strIU7ng strIU7ng_replace(strIU7ng source, strIU7ng search, strIU7ng replace)
  @brief Функция заменяет подстроку search на подстроку replace во всей строке source
 
  @param source [in]
@@ -26,7 +57,7 @@ strIU7ng strIU7ng_replace(strIU7ng source, strIU7ng search, strIU7ng replace)
     if (search.len < 0 || search.head == NULL)
         return source;
 
-    if (strcmp(search.head, replace.head) == 0)
+    if (compare_str(search.head, replace.head) == 0)
         return source;
     
     find = strIU7ng_find_substr(source, search);
