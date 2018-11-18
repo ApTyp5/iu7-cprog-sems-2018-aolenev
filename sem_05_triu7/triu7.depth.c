963fee8d6a621c23016f8528e62eb7d383026252
Gorbunov <darkwen3@mail.ru>
2018-11-19 01:37:13 +0300

    update due to new header

--- sem_05_triu7/triu7.depth.c
+++ sem_05_triu7/triu7.depth.c
@@ -1,41 +1,49 @@
 /**
- @file
+ @file triu7.depth.c
  */
+
 #include "triu7.h"
-#define NULL (void*)0
 
 /**
- @fn void count_depth(int *max, int n, triu7 tree)
+ @fn void count_depth(int *max, int n, triu7_ptr node)
  
- @param max [in, out] - указатель на размер максимальной длины
+ @param max [in, out] - указатель на найденную максимальную глубину
  @param n [in] - текущая глубина
- @param tree [in] - текущее положение в дереве
+ @param node [in] - текущее положение в дереве
  */
-void count_depth(int *max, int n, triu7 *tree)
+void count_depth(int *max, int n, triu7_ptr node)
 {
-    for (int i = 0; i < tree->num_of_leaves; i++)
-        count_depth(max, n + 1, *((tree->leaves) + i));
+    liu7st_element *now = node->leavs->first;
+    
+    while (now != NULL && now->data != NULL)
+    {
+        count_depth(max, n + 1, (triu7_ptr)(now->data));
+        now = now->next;
+    }
     
     if (n > *max)
         *max = n;
 }
 
 /**
- @fn int triu7_depth(triu7 tree)
+int triu7_depth(triu7_ptr node)
  
- @param tree [in] - голова дерева
+ @param node [in] - узел дерева
  
  @return Возвращает глубину дерева
  */
-int triu7_depth(triu7 *tree)
+int triu7_depth(triu7_ptr node)
 {
-    if (tree == NULL)
-        return FAIL;
+    if (node == NULL)
+        return IU7_FAIL;
+    
+    if (triu7_has_cycle(node))
+        return IU7_FAIL;
     
     int size = 0;
     int n = 0;
     
-    count_depth(&size, n, tree);
+    count_depth(&size, n, node);
     
     return size;
 }
