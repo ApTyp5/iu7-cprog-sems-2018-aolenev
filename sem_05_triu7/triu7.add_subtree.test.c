3894041875806dd73ecab40576a311fe7ac57fa4
iai17u666 <andrey3000.99@mail.ru>
2018-11-24 01:05:28 +0300

    update add_subtree tests

--- sem_05_triu7/triu7.add_subtree.test.c
+++ sem_05_triu7/triu7.add_subtree.test.c
@@ -5,8 +5,20 @@ int triu7_add_subtree_test(triu7_ptr root, triu7_ptr sub, int w)
 {
     int res = 0;
 
-    if (triu7_add_subtree(root, sub, w) == 0 && (root->con_wei)[0] == w && root->leavs->first->data == sub)
-        res = 1;
+    if (root == NULL || sub == NULL)
+    {
+        if (triu7_add_subtree(root, sub, w) == IU7_FAIL)
+        {
+            res = 1;
+        }
+    }
+    else
+    {
+        if (triu7_add_subtree(root, sub, w) == IU7_SUCCESS && (root->con_wei)[0] == w && root->leavs->first->data == sub)
+        {
+            res = 1;
+        }
+    }
 
     return res;
 }
@@ -18,8 +30,13 @@ int main(void)
     int data2 = 2;
     triu7_ptr root = triu7_create(&data1);
     triu7_ptr subtree = triu7_create(&data2);
+    int res = 0;
 
-    triu7_add_subtree_test(root, subtree, 3);
+    res += triu7_add_subtree_test(root, subtree, 3);
+    res += triu7_add_subtree_test(root, NULL, 3);
+    res += triu7_add_subtree_test(NULL, subtree, 3);
+
+    printf("Completed tests: %d.", res);
 
     triu7_free(&root);
 
