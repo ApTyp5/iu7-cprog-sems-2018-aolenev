183a64defb55f6a10398f253a3a618a6b2cbc182
Drozdov Vladimir <drozdovvd@student.bmstu.ru>
2018-11-23 23:07:52 +0300

    Added triu7_add()

---
+++ sem_05_triu7/triu7.add.c
@@ -0,0 +1,12 @@
+#include "triu7.h"
+
+int triu7_add(triu7_ptr node, void *new_data, int weight)
+{
+    darriu7 new_weights = darriu7_append(node->con_wei, weight);
+    if (new_weights == NULL)
+        return IU7_FAIL;
+
+    node->con_wei = new_weights;
+    liu7st_append(node->leavs, triu7_create(new_data));
+    return IU7_SUCESS;
+}
