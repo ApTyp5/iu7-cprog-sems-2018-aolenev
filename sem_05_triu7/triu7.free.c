0c6ad65a7072132a17facfdc6ee79ebf868bae6a
iim17u453 <ilyasov9999@gmail.com>
2018-11-23 20:04:20 +0300

    Free func was updated.

--- sem_05_triu7/triu7.free.c
+++ sem_05_triu7/triu7.free.c
@@ -14,21 +14,22 @@ void free_data(void *data)
  */
 void triu7_free(triu7_ptr *root)
 {
-    if (root == NULL || root->data == NULL || root->con_wei == NULL || \
-        root->leavs == NULL)
+    if ((*root) == NULL || (*root)->data == NULL || (*root)->con_wei == NULL || \
+        (*root)->leavs == NULL)
     {
         return;
     }
 
-    liu7st_element *elem = root->leavs->first;
+    liu7st_element *elem = (*root)->leavs->first;
     do
     {
-        triu7_free(elem->data);
+        triu7_free(&elem->data);
         elem = elem->next;
-    } while (elem != root->leavs->last->next);
-    liu7st_free(root->leavs, free_data);
-    darriu7_release(*(root->con_wei));
-    free(root->data);
-    free(root);
-    root = NULL;
+    } while (elem != (*root)->leavs->last->next);
+	
+    liu7st_free((*root)->leavs, free_data);
+    darriu7_release(*((*root)->con_wei));
+    free((*root)->data);
+    free(*root);
+	*root = NULL;
 }
