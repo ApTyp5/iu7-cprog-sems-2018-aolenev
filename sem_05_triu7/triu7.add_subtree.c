848364c001c29bdd565d37a8b20b463d7ac44bdd
iai17u666 <andrey3000.99@mail.ru>
2018-11-24 00:45:19 +0300

    add comments

--- sem_05_triu7/triu7.add_subtree.c
+++ sem_05_triu7/triu7.add_subtree.c
@@ -1,6 +1,13 @@
 #include <triu7.h>
 
-
+/**
+ * @fn triu7_add_subtree(triu7_ptr node, triu7_ptr subroot, int weight)
+ * @brief Добавляет поддерево к узлу
+ * @param node Узел, к которому добавляется поддерево
+ * @param subroot Поддерево
+ * @param weight Вес связи узла с поддеревом
+ * @return Код выполнения
+ */
 int triu7_add_subtree(triu7_ptr node, triu7_ptr subroot, int weight)
 {
     if (node == NULL || subroot == NULL)
