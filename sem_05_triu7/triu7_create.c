b5aa7173af5e610f2518265226744df30fec4bc1
nan17u724 <neknas28@gmail.com>
2018-11-23 20:45:00 +0300

    added memory check in create

--- sem_05_triu7/triu7_create.c
+++ sem_05_triu7/triu7_create.c
@@ -3,6 +3,10 @@
 triu7_ptr triu7_create(void *data)
 {
     triu7_ptr tree = malloc(sizeof(triu7_node));
+
+    if (!tree)
+	return NULL;
+
     tree->data = data;
     tree->con_wei = darriu7_create(0);
     tree->leavs = NULL;
