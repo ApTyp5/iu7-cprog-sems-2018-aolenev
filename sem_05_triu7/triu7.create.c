061d3b5ee51629ff68179f5c258d84ecc3749eec
paa17u852 <aptyp.potatoch@gmail.com>
2018-11-24 14:27:11 +0300

    create func and tests improved

--- sem_05_triu7/triu7.create.c
+++ sem_05_triu7/triu7.create.c
@@ -3,13 +3,21 @@
 triu7_ptr triu7_create(void *data)
 {
     triu7_ptr tree = malloc(sizeof(triu7_node));
-
     if (!tree)
 	return NULL;
 
     tree->data = data;
+
     tree->con_wei = darriu7_create(0);
-    tree->leavs = liu7st_create();
+    if (tree->con_wei == NULL)
+        return NULL;
+
+
+    tree->leavs = malloc(sizeof(liu7st));
+    if (tree->leavs == NULL)
+        return NULL;
+
+    *(tree->leavs) = liu7st_create();
 
     return tree;
 }
--- sem_05_triu7/triu7.create.test.c
+++ sem_05_triu7/triu7.create.test.c
@@ -1,13 +1,23 @@
 #include "triu7.h"
+#include <stdio.h>
 
 int main()
 {
+    printf("Create test started:\n");
+
     int x = 5;
     void *data = &x;
     triu7_ptr tree = triu7_create(data);
     
     if (tree->data == data && tree->leavs->size == 0 && tree->leavs == NULL)
-        return IU7_SUCCESS;
-    
-    return IU7_FAIL;
+    {
+        printf("test passed\n");
+    }
+    else
+    {
+        printf("test not passed\n");
+    }
+
+    return 0;
 }
+
