a6d08bccdc8c90da492d6db4438c56473b530e13
paa17u852 <aptyp.potatoch@gmail.com>
2018-11-24 15:08:16 +0300

    create inproved

--- sem_05_triu7/triu7.create.c
+++ sem_05_triu7/triu7.create.c
@@ -10,12 +10,19 @@ triu7_ptr triu7_create(void *data)
 
     tree->con_wei = darriu7_create(0);
     if (tree->con_wei == NULL)
+    {
+        free(tree);
         return NULL;
+    }
 
 
     tree->leavs = malloc(sizeof(liu7st));
     if (tree->leavs == NULL)
+    {
+        darriu7_release(tree->con_wei);
+        free(tree);
         return NULL;
+    }
 
     *(tree->leavs) = liu7st_create();
 
