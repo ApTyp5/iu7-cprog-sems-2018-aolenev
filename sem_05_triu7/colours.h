ff4d116ddfb6113ae28764175a6ea5eca666ea65
paa17u852 <aptyp.potatoch@gmail.com>
2018-12-08 10:16:17 +0300

    a colours header added

---
+++ sem_05_triu7/colours.h
@@ -0,0 +1,17 @@
+
+#ifndef __COLOURS_H__
+#define __COLOURS_H__
+
+#define BLACK       "\x1b[30m"
+#define RED         "\x1b[31m"
+#define GREEN       "\x1b[32m"
+#define YELLOW      "\x1b[33m"
+#define BLUE        "\x1b[34m"
+#define DRED        "\x1b[35m"
+#define LBLUE       "\x1b[36m"
+#define WHITE       "\x1b[37m"
+
+
+#define END_C       "\x1b[0m"
+
+#endif
