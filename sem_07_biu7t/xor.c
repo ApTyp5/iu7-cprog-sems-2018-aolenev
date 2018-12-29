e6dac5ae5fa2b71653d6d5089e4c12484e554412
Dmitry Pudov <dimapudov99@gmail.com>
2018-12-29 16:36:20 +0300

    Without memory leaks

--- sem_07_biu7t/xor.c
+++ sem_07_biu7t/xor.c
@@ -1,19 +1,38 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <string.h>
 //XOR cipher
 
 char* xor_chiper(char* text, char* key)
 {
-    return NULL;
+    if (text == NULL || key == NULL)
+    {
+        return NULL;
+    }
+
+    int key_len = strlen(key);
+    char *result = calloc((strlen(text) + 1), sizeof(char));
+    if (result != NULL)
+    {
+        int i = 0;
+        for (; i < strlen(text); i++)
+        {
+            result[i] = text[i] ^ key[i % key_len];
+        }
+        result[i] = '\0';
+    }
+    return result;
 }
 
 int main(void)
 {
     char* encoded = xor_chiper("Hello world", "pa$$w0rd");
-    printf("%s", encoded);
+    printf("%s\n", encoded);
 
-    char* decoded = xor_chiper(encoded "pa$$w0rd");
-    printf("%s", encoded);
+    char* decoded = xor_chiper(encoded, "pa$$w0rd");
+    printf("%s\n", decoded);
 
-    return 1;
+    free(encoded);
+    free(decoded);
+    return 0;
 }
