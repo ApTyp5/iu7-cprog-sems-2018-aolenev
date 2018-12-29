8d8c1ed2469611870e833d29a5e2ae4f4ed09399
Stepanov Alexandr <happymainer@gmail.com>
2018-12-29 16:24:45 +0300

    EDIT: xor.c

--- sem_07_biu7t/xor.c
+++ sem_07_biu7t/xor.c
@@ -1,19 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 //XOR cipher
 
+int len(char *str)
+{
+    int n = 0;
+    while (str[n] != '\0') n++;
+    return n;
+}
+
 char* xor_chiper(char* text, char* key)
 {
-    return NULL;
+    int len_text = len(text);
+    int len_key = len(key);
+
+    char *result = malloc(len_text + 1);
+
+    for (int i = 0; i < len_text; i++)
+        result[i] = text[i] ^ key[i % len_key];
+
+    result[len_text] = '\0';
+
+    return result;
 }
 
 int main(void)
 {
     char* encoded = xor_chiper("Hello world", "pa$$w0rd");
-    printf("%s", encoded);
+    printf("%s\n", encoded);
 
     char* decoded = xor_chiper(encoded, "pa$$w0rd");
-    printf("%s", decoded);
+    printf("%s\n", decoded);
+
+    free(encoded);
 
     return 1;
 }
