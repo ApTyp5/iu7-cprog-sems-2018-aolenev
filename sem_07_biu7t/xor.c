d8ce4552703021eee6d1df065f439356532a7b12
sag17u762 <nasty.sidenko@mail.ru>
2018-12-29 16:20:21 +0300

    Added xor

--- sem_07_biu7t/xor.c
+++ sem_07_biu7t/xor.c
@@ -1,19 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 //XOR cipher
 
 char* xor_chiper(char* text, char* key)
 {
-    return NULL;
+    int i = 0, j = 0;
+    char *new = calloc(20, sizeof(char));
+
+    while (text[i] != '\0')
+    {
+        new[i] = text[i] ^ key[j];
+        if (key[j] != '\0')
+            j = 0;
+        i++;
+        j++;
+    }
+    new[i] = '\0';
+    return new;
 }
 
 int main(void)
 {
     char* encoded = xor_chiper("Hello world", "pa$$w0rd");
-    printf("%s", encoded);
+    puts(encoded);
 
     char* decoded = xor_chiper(encoded, "pa$$w0rd");
-    printf("%s", decoded);
+    puts(decoded);
 
     return 1;
 }
