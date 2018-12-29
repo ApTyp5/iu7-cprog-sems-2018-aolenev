e051b7191b656bdce6eb360b14dc13a928994062
geonerus <geonerus@mail.ru>
2018-12-29 16:23:53 +0300

    f

--- sem_07_biu7t/xor.c
+++ sem_07_biu7t/xor.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
+#include <string.h>
+#include <malloc.h>
 
 //XOR cipher
 
-char* xor_chiper(char* text, char* key)
+char* xor_chiper(char* text, char* key )
 {
-    return NULL;
+
+  char * output;
+  output=malloc(strlen(text));
+  int i;
+  for(i = 0; i < strlen(text); i++) {
+    output[i] = text[i] ^ key[i % (sizeof(key)/sizeof(char))];
+  }
+  return output;
 }
 
 int main(void)
 {
+
     char* encoded = xor_chiper("Hello world", "pa$$w0rd");
     printf("%s", encoded);
 
-    char* decoded = xor_chiper(encoded "pa$$w0rd");
-    printf("%s", encoded);
+    char* decoded = xor_chiper(encoded, "pa$$w0rd");
+    printf("%s", decoded);
 
     return 1;
 }
