161b3d22a6f25fb0b26f2d000467e73d7562b19e
iim17u453 <ilyasov9999@gmail.com>
2018-12-29 16:27:06 +0300

    Add Idris variant.

--- sem_07_biu7t/xor.c
+++ sem_07_biu7t/xor.c
@@ -1,19 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 //XOR cipher
 
+int len_str(char *str)
+{
+    int len = 0;
+
+    while (str[len] != '\0')
+    {
+        len += 1;
+    }
+
+    return len;
+}
+
 char* xor_chiper(char* text, char* key)
 {
-    return NULL;
+    int len;
+    char *result;
+    result = malloc(20 * sizeof(int));
+    len = len_str(text);
+    int j = 0;
+
+    for (int i = 0; i < len; i++)
+    {
+        if (i == len_str(key))
+        {
+            j = 0;
+        }
+
+        result[i] = text[i] ^ key[j];
+        ++j;
+    }
+
+    return result;
 }
 
 int main(void)
 {
     char* encoded = xor_chiper("Hello world", "pa$$w0rd");
-    printf("%s", encoded);
+    
+    for (int i = 0; encoded[i] != '\0'; i++)
+	    printf("%d\n", encoded[i]);
 
-    char* decoded = xor_chiper(encoded "pa$$w0rd");
-    printf("%s", encoded);
+    char* decoded = xor_chiper(encoded, "pa$$w0rd");
+    printf("%s", decoded);
 
     return 1;
 }
